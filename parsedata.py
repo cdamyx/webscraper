5118446771df45f18b0808206c955e9e6cb97bd2
Chris Amyx <40224280+cdamyx@users.noreply.github.com>
2020-02-19 11:35:57 -0600

    tinkering with parser

--- parsedata.py
+++ parsedata.py
@@ -6,5 +6,5 @@ with open('twitterData.json') as json_data:
     jsonData = json.load(json_data)
 
     for i in jsonData:
-        #if i['date'] < testDate:
-        print(i['date'])
+        if i['tweet'] == "Michael Fassbender and I have an Air Guitar Battle https://t.co/LZ6Syju36t #FallonTonight":
+            print(i['date'])
